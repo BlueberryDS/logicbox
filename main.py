4a5d4cb654193c1e4e76f8778ac566c137baac1f
Aruniscool <41237832+Aruniscool@users.noreply.github.com>
2018-07-29 13:20:10 -0400

    If answer is equal to 0 than all previous answers will show, if answer is equal to 100 than print boom!

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from logic import *
+sum_list = []
 
-#Start your program here
-
-a = 1
-
-def b():
-    global a
-    a += 2
-    print( " " + str(a))
-
-b()
-print(a)
+while True:
+    number_1 = int(input ("What is number 1"))
+    number_2 = int(input ("What is number 2"))
+    print(number_1 + number_2)
+    answer = (number_1 + number_2)
+    if answer != 0:
+        sum_list.append(answer)
+    if answer == 0:
+        for y in sum_list:
+            print(y)
+    if answer == 100:
+        print("Boom!")
