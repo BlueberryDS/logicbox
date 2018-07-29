f29be70f085244395f2d767946ff8d2c2121bcf1
pragieth <pragiethsuresh@gmail.com>
2018-07-29 13:20:52 -0400

    I have gotten better by laening with you in programing Thank you

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from logic import *
-
-#Start your program here
-
-a = 1
-
-def b():
-    global a
-    a += 2
-    print( " " + str(a))
-
-b()
-print(a)
+while True:
+    number_1 = int(input("what is the number 1 "))
+    number_2 = int(input("what is the number 2 "))
+    print(number_1 + number_2)
+    if number_1 + number_2 >= 100:
+        print("Boom")
+        if  number_1 + number_2 >= 0:
+            print
