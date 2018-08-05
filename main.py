83cfa37d29f3eca97787cb4e738bac44690c06f3
pravinemani <pravine.mani@gmail.com>
2018-08-05 12:19:11 -0400

    Homework completed. This program adds and calculates sum of two number. If the number is greater than or equal to 100 it prints boom and if 0 it shows a list of all the sums calculated.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from logic import *
 
-#Start your program here
+list_of_sums = []
 
-a = 1
+while True:
 
-def b():
-    global a
-    a += 2
-    print( " " + str(a))
+    print("Enter you First number")
+    number_1 = int(input("Age: "))
 
-b()
-print(a)
+    print("Enter you Second number")
+    number_2 = int(input("Age: "))
+
+    num_Sum = number_1 + number_2
+
+    print("The sum is: " + str(num_Sum))
+
+    list_of_sums = list_of_sums + [num_Sum]
+
+    if num_Sum >= 100:
+        print("BOOOOM")
+
+    elif num_Sum == 0:
+        for item in list_of_sums:
+            print(item)
+
+
+
