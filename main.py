79210ac87af65712f9a3a6d2fc1b24a40c9d734f
Mathumeethaa <mathumeethaa@gmail.com>
2018-08-12 14:34:48 -0400

    my new commit i didnt do anything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-while True:
-    number1 = int(input("what is the number 1 "))
-    number2 = int(input("what is the number 2 "))
-    print(number1 + number2)
 
-    if number1 + number2 == 100:
-        print("boom")
+
+
+
+
+
