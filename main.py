f57059f58fd6a8505f412abf0b471e8db642f749
Aruniscool <41237832+Aruniscool@users.noreply.github.com>
2018-08-12 14:34:58 -0400

    Arun's test commiting things

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 sum_list = []
 
 while True:
-    number_1 = int(input ("What is number 1"))
-    number_2 = int(input ("What is number 2"))
+    number_1 = int(input("What is number 1"))
+    number_2 = int(input("What is number 2"))
     print(number_1 + number_2)
     answer = (number_1 + number_2)
     if answer != 0:
