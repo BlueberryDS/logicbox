9eaa3f19ef5e2c39372e8cb8aeff7c4c9be771d8
Aruniscool <41237832+Aruniscool@users.noreply.github.com>
2018-08-12 16:00:39 -0400

    Program that unlocks one logic box

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from logic import *
 
-#Start your program here
+box = get_new_logic_box()
+analyser = get_new_analyser()
+unlocker = get_new_unlocker()
 
-a = 1
+type = (analyser.analyse(box))
+print("The type of logic box" + type)
+if type == "COMBO":
+        while not box.is_unlocked():
 
-def b():
-    global a
-    a += 2
-    print( " " + str(a))
+            box.flip()
 
-b()
-print(a)
+            box.interact("TAP")
+        results = unlocker.unbox(box)
+        print(results)
