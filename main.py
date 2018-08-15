00a3c2413e2227b1bb14e28519c38bd02a5f23f4
pravinemani <pravine.mani@gmail.com>
2018-08-14 23:35:06 -0400

    Completed program which unlocks all logicboxes

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ analyser = get_new_analyser()
 unlocker = get_new_unlocker()
 
 type = analyser.analyse(box)
+index = 0
+list_of_content = []
 
 print("The type of box is " + type)
 
@@ -14,5 +16,35 @@ if type == "COMBO":
     while not box.is_unlocked():
         box.flip()
         box.interact("TAP")
-    content = unlocker.unbox(box)
-    print(content)
+    list_of_content.append(unlocker.unbox(box))
+
+while not is_game_done():
+
+    for boxes in list_of_content[index]:
+
+        type = analyser.analyse(boxes)
+        print("The type of box is " + type)
+
+        if type == "COMBO":
+            while not boxes.is_unlocked():
+                boxes.flip()
+                boxes.interact("TAP")
+            list_of_content.append(unlocker.unbox(boxes))
+
+        elif type == "OMNI":
+            list_of_content.append(unlocker.unbox(boxes))
+        print(list_of_content)
+
+    for content in list_of_content:
+
+        for boxes in content:
+
+            type = analyser.analyse(boxes)
+
+            if type == "COMBO":
+                list_of_content.append(unlocker.unbox(boxes))
+
+            elif type == "OMNI":
+                list_of_content.append(unlocker.unbox(boxes))
+
+    index += 1
